f93a95c3883886020e8895814b2f0e4230ff8c57
Carlos Hernandez <chern042@uottawa.ca>
2018-12-03 18:23:48 -0500

    Made the availabilities for the weekday chosen to pop up. Can select them individually, turning them RED, or back to their default color. Still need to implement a way to select the red ones in the chosen week, and save the week and availabilites to database under correct format.

--- app/src/main/java/com/steam/appseg2105/SelectAvailabilities.java
+++ app/src/main/java/com/steam/appseg2105/SelectAvailabilities.java
@@ -1,9 +1,11 @@
 package com.steam.appseg2105;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.os.Bundle;
 import android.view.View;
@@ -22,9 +24,8 @@ import java.util.List;
 
 
 public class SelectAvailabilities extends AppCompatActivity {
-    private ListView availList;
-    private List<String> serviceList;
-    private TextView mondayToFriday;
+    private List<String> hoursList;
+    private TextView serviceTitle;
     private TextView monday;
     private TextView tuesday;
     private TextView wednesday;
@@ -33,11 +34,10 @@ public class SelectAvailabilities extends AppCompatActivity {
     private TextView saturday;
     private TextView sunday;
     private ListView weeksList;
-    private TextView testt;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_availabilities);
-        mondayToFriday = findViewById(R.id.mondayToFriday);
+        serviceTitle = findViewById(R.id.serviceTitle);
         monday = findViewById(R.id.monday);
         tuesday = findViewById(R.id.tuesday);
         wednesday = findViewById(R.id.wednesday);
@@ -46,7 +46,6 @@ public class SelectAvailabilities extends AppCompatActivity {
         saturday = findViewById(R.id.saturday);
         sunday = findViewById(R.id.sunday);
         weeksList = findViewById(R.id.weeksList);
-        testt = findViewById(R.id.test);
 
 
         FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -61,6 +60,28 @@ public class SelectAvailabilities extends AppCompatActivity {
                             listy.add(availSnapshot.getKey());
                         }
                         createList(listy);
+                        serviceTitle.setText(getIntent().getStringExtra("service")+" Hours");
+                        weeksList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                final String week = (String) adapterView.getItemAtPosition(i);
+                                int defaultColor = serviceTitle.getCurrentTextColor();
+                                monday.setText("Monday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("monday").getValue().toString());
+                                tuesday.setText("Tuesday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("tuesday").getValue().toString());
+                                wednesday.setText("Wednesday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("wednesday").getValue().toString());
+                                thursday.setText("Thursday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("thursday").getValue().toString());
+                                friday.setText("Friday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("friday").getValue().toString());
+                                saturday.setText("Saturday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("saturday").getValue().toString());
+                                sunday.setText("Sunday: " + snapshot.child("Availabilities").child(getIntent().getStringExtra("service")).child(week).child("sunday").getValue().toString());
+                                selectWeek(monday, defaultColor);
+                                selectWeek(tuesday, defaultColor);
+                                selectWeek(wednesday,defaultColor);
+                                selectWeek(thursday,defaultColor);
+                                selectWeek(friday,defaultColor);
+                                selectWeek(saturday,defaultColor);
+                                selectWeek(sunday,defaultColor);
+                            }
+                        });
                     }
                 }
             }
@@ -75,6 +96,18 @@ public class SelectAvailabilities extends AppCompatActivity {
     }
 
 
+    private void selectWeek(final TextView weekday, final int defaultColor){
+        weekday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(weekday.getCurrentTextColor()==defaultColor){
+                    weekday.setTextColor(Color.RED);
+                }else{
+                    weekday.setTextColor(defaultColor);
+                }
+            }
+        });
+    }
     public void createList(ArrayList list) {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         weeksList.setAdapter(adapter);
